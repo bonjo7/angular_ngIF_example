ca4e799107e8c76ffba8bb86eadde81c68f8b992
Bernard Thompson <bethomps@localhost.localdomain>
2019-05-22 15:16:37 +0100

    Adding comments and tidying up ngif condition

--- src/app/ngif/ngif.component.ts
+++ src/app/ngif/ngif.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ngif.component.css']
 })
 export class NgifComponent implements OnInit {
+  //Setting marked to false which will be used as ngIF condition in html to display or not display paragraph component
   marked = false;
+  //Set checkbox to false to ensure if is unticked
   theCheckbox = false;
   constructor() { }
 
+  //Using toggle method to change what will be displayed
   toggleVisibility(e){
     this.marked= e.target.checked;
   }
